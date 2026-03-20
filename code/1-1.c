2b44d76e700cebe97b3fc130b70360773547d2de
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-20 10:12:03 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
+void add(int *a, int *b) {
+  int k;
+  k = *a + *b;
+  printf("%d + %d = %d\n", *a, *b,  k);
+}
+void sub(int *a, int *b) {
+  int k;
+  k = *a - *b;
+  printf("%d - %d = %d\n", *a, *b,  k);
+}
+void mul(int *a, int *b) {
+  int k;
+  k = *a * *b;
+  printf("%d * %d = %d\n", *a, *b,  k);
+}
+void div(int *a, int *b) {
+  int k;
+  k = *a / *b;
+  printf("%d / %d = %d\n", *a, *b,  k);
+}
 
 
 int main(void) {
+  int a = 10, b = 5;
+  add(&a, &b);
+  sub(&a, &b);
+  mul(&a, &b);
+  div(&a, &b);
 
   return 0;
 }
