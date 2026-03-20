e7ea62ba8101115fab693fe50a26f1f66829b594
TaeminHan0302 <tefzen0302@gmail.com>
2026-03-20 16:57:30 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void my_sum(char type, int count, ...) {
+  va_list args;
+  va_start(args, count);
+
+  if (type == 'S') {
+    for(int i = 0; i < count; i++) {
+      char *str = va_arg(args, char*);
+      printf("%s", str);
+      if (i < count -1) {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+  else if (type == 'C') {
+    for(int i = 0; i < count; i++) {
+      int c = va_arg(args, int);
+      printf("%c", c);
+    }
+    printf("\n");
+  }
+  else if (type == 'D') {
+    int sum = 0;
+    for(int i = 0; i < count; i++) {
+      sum += va_arg(args, int);
+    }
+    printf("%d\n", sum);
+  }
+  else {
+    printf("\n");
+  }
+  va_end(args);
+
+}
+int main() {
+  my_sum('S', 2, "Hello", "World");
+  my_sum('C', 3, 'C', 'N', 'U');
+  my_sum('D', 4, 10, 20, 30, 40);
+  return 0;
+}
